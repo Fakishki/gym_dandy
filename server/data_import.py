aa7e2b922634886fa97c3f2da2944415214e9dca
Wally Pfingsten <wallypfingsten@gmail.com>
2023-05-31 03:08:28 -0700

    strength import queued up

--- server/data_import.py
+++ server/data_import.py
@@ -5,13 +5,22 @@ from models import db, Workout, StrengthExercise, CardioExercise, Strength, Card
 
 import pandas as pd
 
-df = pd.read_excel('./imports/wally_workouts.xlsx')
+df = pd.read_excel('./imports/wally_strengths.xlsx')
+
+# with app.app_context():
+#     for index, row in df.iterrows():
+#         workout = Workout(user_id=row['user_id'], created_at=row['created_at'])
+
+#         db.session.add(workout)
+
+#     db.session.commit()
+# print("Workouts added")
 
 with app.app_context():
     for index, row in df.iterrows():
-        workout = Workout(user_id=row['user_id'], created_at=row['created_at'])
+        strength = Strength(name=row['name'], equipment=row['equipment'], favorite=row['favorite'])
 
-        db.session.add(workout)
+        db.session.add(strength)
 
     db.session.commit()
-print("Workouts added")
+print("Strengths added")
